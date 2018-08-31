379d4d91910e7be039b5eba3f6f488140a16681f
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-31 15:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function createPost() {
-  let title = document.getElementById("")
-  let body =
-  let author =
+  let title = document.getElementById("postTitle");
+  let body = document.getElementById("postBody");
+  let author = document.getElementById("postAuthor");
 }
